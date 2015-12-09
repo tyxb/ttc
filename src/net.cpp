b39c67c9441e796defff689460d12b1f21a640de
tyxb <postmaster@tianyuan-c.com>
2015-12-09 17:04:05 +0800

    update
    update

--- src/net.cpp
+++ src/net.cpp
@@ -1149,7 +1149,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-  {"121.42.196.77", "121.42.196.77"}
+  {"127.0.0.1", "127.0.0.1"}
 	
 };
 
